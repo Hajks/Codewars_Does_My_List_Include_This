25ec928b37036e146f6226fe5834e9d30329c8a4
Hajk <tunczorx@gmail.com>
2018-10-17 12:16:08 +0200

    Add project files.

---
+++ Codewars_Does_My_List_Include_This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars_Does_My_List_Include_This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
